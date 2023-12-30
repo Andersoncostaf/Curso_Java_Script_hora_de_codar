1d6b95aebc2c3679aa771b692a74ec88da4712b4
Anderson Costa <andersoncosta1801@gmail.com>
2023-12-30 20:30:27 -0300

    Adicionando pasta 7, exercicio 24, de exercicios sobre funções

---
+++ 7_EXERCICIOS_funcoes/9_exercicio_24/scripts.js
@@ -0,0 +1,13 @@
+function potencia(a,b) {
+  // return Math.pow(a,b);
+  return a ** b;
+}
+
+console.log(potencia(2,2));
+console.log(potencia(3,2));
+console.log(potencia(4,4));
+console.log(potencia(5,2));
+
+
+//30/12
+
