cb302561e00b2d681bd54e7c1436d7827eabbf1d
Anderson Costa <andersoncosta1801@gmail.com>
2023-12-30 18:09:17 -0300

    Adicionando pasta 7, exercicio 20, de exercicios sobre funções

---
+++ 7_EXERCICIOS_funcoes/5_exercicio_20/scripts.js
@@ -0,0 +1,20 @@
+function entrarNaAutoEscola(idade) {
+
+  if(idade >= 18) {
+    console.log("Pode matricular-se na auto escola");
+
+  } else {
+    console.log("Não pode matricular-se na auto escola");
+  }
+
+}
+
+entrarNaAutoEscola(16);
+entrarNaAutoEscola(17);
+entrarNaAutoEscola(18);
+entrarNaAutoEscola(25);
+entrarNaAutoEscola(15);
+entrarNaAutoEscola(23);
+entrarNaAutoEscola(28);
+
+// 20/12
