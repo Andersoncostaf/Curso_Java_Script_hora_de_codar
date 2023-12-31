d4b3ada17abd4cfb946419b52eecfff6488b97fe
Anderson Costa <andersoncosta1801@gmail.com>
2023-12-30 21:01:07 -0300

    Adicionando pasta 7, exercicio 25, de exercicios sobre funções

---
+++ 7_EXERCICIOS_funcoes/10_exercicio_25/scripts.js
@@ -0,0 +1,13 @@
+function imprimePar(num) {
+  for(let i = num; i >= 0; i--) {
+    if(i % 2 == 0) {
+      console.log(i);
+    }
+  }
+}
+
+imprimePar(15);
+imprimePar(23);
+imprimePar(48);
+
+// 30/12
---
+++ 7_EXERCICIOS_funcoes/11_EXERCICIO6_FUNCAO_DE_SOMA/Course_ JavaScript do básico ao avançado (c_ Node.js e projetos) _ Udemy_files/common-desktop-server-side.e71dd4589cb289918cb3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["common-desktop-server-side"],[]]);
+//# sourceMappingURL=common-desktop-server-side.e71dd4589cb289918cb3.js.map
