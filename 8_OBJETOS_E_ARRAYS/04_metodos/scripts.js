27c7487e6e251604945a1913031114158b45798d
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-01 18:46:26 -0300

    Adicionando pasta 8, atualização ,sobre objetos e Arrays

---
+++ 8_OBJETOS_E_ARRAYS/04_metodos/scripts.js
@@ -0,0 +1,10 @@
+let marca = 'Adidas';
+console.log(marca.toUpperCase());
+
+let marca2 = marca.toLocaleUpperCase();
+console.log(marca2.toLowerCase());
+
+console.log(typeof marca2.toLocaleLowerCase);
+
+// string.propriedade
+// string.metodo()
