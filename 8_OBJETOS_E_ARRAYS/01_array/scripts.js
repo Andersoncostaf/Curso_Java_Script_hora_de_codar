a0b18052c4017828870eb33ab15b78c8a978320c
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-01 18:30:27 -0300

    Adicionando pasta 8, atualização ,sobre objetos e Arrys

---
+++ 8_OBJETOS_E_ARRAYS/01_array/scripts.js
@@ -0,0 +1,12 @@
+let arr = [1, 4, 5, 6, 7];
+let nomes = ["Anderson", "João", "Aléxia"];
+let bool = [true, false, true];
+
+let misturado = [1, 'Anderson', true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(misturado);
+
+// 01/01
