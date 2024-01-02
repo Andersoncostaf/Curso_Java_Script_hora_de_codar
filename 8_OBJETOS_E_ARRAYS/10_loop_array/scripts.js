5371c0669fef5872d8ad6d6d1db4b91995cf0849
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-02 12:03:02 -0300

    Adicionando pasta 8, atualização ,sobre objetos e Arrays

--- 8_OBJETOS_E_ARRAYS/10_loop_array/scripts.js
+++ 8_OBJETOS_E_ARRAYS/10_loop_array/scripts.js
@@ -1,5 +1,5 @@
 let nomes = ["Anderson", "Danilo", "Jose"];
 
-for(let i = 0; i <= nome.length; i++) {
+for(let i = 0; i <= nomes.length; i++) {
   console.log(nomes[i]);
 }
---
+++ 8_OBJETOS_E_ARRAYS/11_push_pop/scripts.js
@@ -0,0 +1,9 @@
+let times = ["Flamengo", "Paysandu", "Jaragua"];
+
+let elementoRemovido = times.pop();
+
+console.log(elementoRemovido);
+console.log(times);
+
+times.push('Ceará');
+console.log(times);
