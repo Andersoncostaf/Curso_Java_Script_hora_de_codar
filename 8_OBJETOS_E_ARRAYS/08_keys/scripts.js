fd247b45b56c729323a3bae184858a5397e98f3d
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-02 11:25:06 -0300

    Adicionando pasta 8, atualização ,sobre objetos e Arrays

--- 8_OBJETOS_E_ARRAYS/08_keys/scripts.js
+++ 8_OBJETOS_E_ARRAYS/08_keys/scripts.js
@@ -2,9 +2,13 @@ let objeto = {
   'chave1': 1,
   'chave1': 2,
   'chave1': 3,
+  //'chave1': [1, 2, 3],
 }
 
 console.log(objeto);
 
 console.log(Object.keys(objeto));
+
+//imprime o primeiro
+//pq em jave script as chaves devem ser unicas
 
