c0fced61daf1b66402ff5e5b995b9d701552cf04
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-02 11:05:22 -0300

    Adicionando pasta 8, atualização ,sobre objetos e Arrays

---
+++ 8_OBJETOS_E_ARRAYS/07_assign/scripts.js
@@ -0,0 +1,19 @@
+
+
+let carro = {
+  portas: 2,
+  portamalas: '200 litros',
+  motor: '2.0'
+}
+
+console.log(carro);
+
+let adicionais = {
+  tetosolar: true,
+  arcondicionado: true,
+}
+
+Object.assign(carro, adicionais);
+console.log(carro);
+
+
