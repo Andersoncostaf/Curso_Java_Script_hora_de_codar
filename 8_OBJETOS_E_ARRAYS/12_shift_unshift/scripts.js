a3de6ee35b6a937132023d8aa7074e2048f68b99
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-02 12:15:02 -0300

    Adicionando pasta 8, atualização ,sobre objetos e Arrays

---
+++ 8_OBJETOS_E_ARRAYS/12_shift_unshift/scripts.js
@@ -0,0 +1,10 @@
+let carros = ["Honda Civic", "Audi", "VW", "Fiati"];
+
+let removerPrimeiroCarro = carros.shift();
+console.log(removerPrimeiroCarro);
+console.log(carros);
+
+carros.unshift('Ferrari');
+
+console.log(carros);
+console.log(carros[0]);
