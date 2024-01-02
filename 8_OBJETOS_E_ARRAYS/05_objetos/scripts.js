835b77633258214253f54ce4a9e108e1d3113c60
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-02 10:32:10 -0300

    Adicionando pasta 8, atualização ,sobre objetos e Arrays

---
+++ 8_OBJETOS_E_ARRAYS/05_objetos/scripts.js
@@ -0,0 +1,30 @@
+let flamengo = {
+  lateral: 2,
+  nome: 'Leo Moura',
+  chute: function() {
+    console.log(" Cruzamento");
+  }
+};
+
+console.log(flamengo.lateral);
+console.log(flamengo.nome);
+flamengo.chute();
+
+
+
+
+
+let cachorro = {
+  patas: 4,
+  nome: 'Shark',
+  latir: function() {
+    console.log("Au Au");
+  }
+};
+
+console.log(cachorro.patas);
+console.log(cachorro.nome);
+cachorro.latir();
+
+// [] = array
+// {} = obj
