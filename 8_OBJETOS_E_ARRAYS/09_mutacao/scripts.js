a04edbbb340557e5173667d81040c642ca28f87c
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-02 11:36:19 -0300

    Adicionando pasta 8, atualização ,sobre objetos e Arrays

--- 8_OBJETOS_E_ARRAYS/09_mutacao/scripts.js
+++ 8_OBJETOS_E_ARRAYS/09_mutacao/scripts.js
@@ -16,4 +16,9 @@ pessoa2.nome = "Pedro";
 console.log(pessoa.nome);
 
 pessoa.nome = "Beatriz";
-console.log(pessoa2.nome);
+console.log(pessoa2.nome); // "beatriz"
+
+/*
+Em resumo, quando você atribui um objeto a outra variável em JavaScript, 
+você está criando uma referência a esse objeto, não uma cópia independente. Alterações feitas através de uma referência afetam o objeto subjacente e são refletidas através de outras referências a esse objeto.
+*/
