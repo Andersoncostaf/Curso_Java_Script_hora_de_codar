2e6410dc98d9f4eda0ea36f50d13eba07a0ea8bb
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-02 10:54:38 -0300

    Adicionando pasta 8, atualização ,sobre objetos e Arrays

---
+++ 8_OBJETOS_E_ARRAYS/06_objetos_criar_prop/scripts.js
@@ -0,0 +1,14 @@
+let pessoa = {
+  nome: 'Anderson',
+  idade: 35,
+  profissao: "Developer",
+}
+
+console.log(pessoa.nome);
+delete pessoa.nome;
+
+console.log(pessoa.nome);
+console.log(pessoa);
+pessoa.casado = true;
+console.log(pessoa.casado);
+
