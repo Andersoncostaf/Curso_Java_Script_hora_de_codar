f0c97bb05e27dd3a2901490626c7585019df0ab9
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-03 11:48:12 -0300

    funcionalidade executada

---
+++ 8_OBJETOS_E_ARRAYS/21_join/scripts.js
@@ -0,0 +1,21 @@
+let frase = "Testando o método split"
+
+let palavras = frase.split(" ");
+
+let novaFrase = palavras.join("@");
+
+console.log(novaFrase);
+console.log(palavras.join(" "));
+
+/*
+
+let frase = "Testando o método split";
+
+let palavras = frase.split(" ");
+
+let novaFrase = palavras.join("@");
+
+console.log(novaFrase);
+
+console.log(palavras.join(" "));
+*/
