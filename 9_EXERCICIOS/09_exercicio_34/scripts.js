90c1b573e94ef7197e5b5acac1e1d4cc646885d0
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-03 15:47:48 -0300

    funcionalidade executada e atualizada

---
+++ 9_EXERCICIOS/09_exercicio_34/scripts.js
@@ -0,0 +1,15 @@
+const fraseS = "Eu quero, eu posso, eu consigo";
+const arrayFrase = fraseS.split(" ");
+
+for(let i = 0; i < arrayFrase.length; i++) {
+  console.log(arrayFrase[i]);
+}
+
+
+const frase = "Ser um programador bom requer muita prática";
+
+const arrFrase = frase.split(" ");
+
+for(let i = 0; i < arrFrase.length; i++) {
+  console.log(arrFrase[i]);
+}
