331eca7cc76455f86927afb3af897307cb3cd039
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-03 11:42:17 -0300

    funcionalidade executada

---
+++ 8_OBJETOS_E_ARRAYS/20_split/scripts.js
@@ -0,0 +1,33 @@
+// Definindo uma string
+let frase = "Testando o método split Anderson Fox";
+
+// Utilizando o método split para dividir a string em um array de palavras, usando o espaço como delimitador
+console.log(frase.split(" "));
+
+// Atribuindo o resultado a uma variável e imprimindo o array de palavras
+let palavras = frase.split(" ");
+console.log(palavras);
+
+// Definindo uma string contendo produtos separados por ponto e vírgula
+let produtos = "Banana;Maça;Jaca;Pera;Bola;Tapete";
+
+// Utilizando o método split para dividir a string em um array de produtos, usando o ponto e vírgula como delimitador
+console.log(produtos.split(';'));
+
+
+
+
+/*
+
+let frase = "Testando o método split";
+
+console.log(frase.split(" "));
+
+let palavras = frase.split(" ");
+
+console.log(palavras);
+
+let produtos = "Banana;Maçã;Jaca;Pera;Bola;Tapete";
+
+console.log(produtos.split(';'));
+*/
