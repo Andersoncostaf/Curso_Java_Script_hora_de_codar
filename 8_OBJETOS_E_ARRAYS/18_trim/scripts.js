aa0d4e4faa368aa4f3ac1733743439f6fe2d3447
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-03 11:21:37 -0300

    funcionalidade executada, da aula 08

---
+++ 8_OBJETOS_E_ARRAYS/18_trim/scripts.js
@@ -0,0 +1,9 @@
+let nome = " \n   Anderson \n";
+// Olá Andersom , tudo bem ?
+
+let nomeCorrigido = nome.trim();
+console.log(nome);
+console.log(nomeCorrigido);
+
+//tira todos os espaços "indesejados"
+
