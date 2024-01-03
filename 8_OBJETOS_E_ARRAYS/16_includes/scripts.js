dea54660ad69e3e276a7be26a6d0129932c23c45
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-03 11:12:09 -0300

    funcionalidade executada

---
+++ 8_OBJETOS_E_ARRAYS/16_includes/scripts.js
@@ -0,0 +1,8 @@
+// Criando um array de carros
+let carros = ["BMW", "Fiat", "Renault", "Audi, VW"];
+
+// Verificando se o array inclui o carro "Fiat" e imprimindo o resultado
+console.log(carros.includes("Fiat")); // Saída: true
+
+// Verificando se o array inclui o carro "Ford" e imprimindo o resultado
+console.log(carros.includes("Ford")); // Saída: false
