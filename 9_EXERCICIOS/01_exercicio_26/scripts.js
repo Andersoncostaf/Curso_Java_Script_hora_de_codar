0dca2e3dc759e75548a6df56bc6c1891bb637961
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-03 14:33:04 -0300

    funcionalidade executada e atualizada

---
+++ 9_EXERCICIOS/01_exercicio_26/scripts.js
@@ -0,0 +1,11 @@
+// Criando um array de números
+let numeros = [5, 10, 15, 20, 25];
+
+// Imprimindo o valor do primeiro elemento do array
+console.log(numeros[0]); // Saída: 5
+
+// Imprimindo o valor do terceiro elemento do array
+console.log(numeros[2]); // Saída: 15
+
+// Imprimindo o valor do quarto elemento do array
+console.log(numeros[3]); // Saída: 20
---
+++ 9_EXERCICIOS/02_exercicio_27/scripts.js
@@ -0,0 +1,9 @@
+let nome = ["Anderson", "Beatriz"];
+let teste = [true, false, true, true];
+
+console.log(nome.length); //saida 2
+console.log(teste.length); //saida 4
+
+
+// length SEMPRE LEMBRAR -->  
+// conta a quandidade de elemento.
