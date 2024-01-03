b30c6a4c4896267dda003449970aad3c0421176e
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-03 15:34:43 -0300

    funcionalidade executada e atualizada

---
+++ 9_EXERCICIOS/07_exercicio_32/scripts.js
@@ -0,0 +1,12 @@
+let timesFutebol = [ "Flamengo", "Tuna", "Paysandu", "Vasco", "Bostafogo"];
+
+for(let i = 0; i < timesFutebol.length; i++) {
+  console.log(timesFutebol[i]);
+}
+
+
+let frutas = ["Maçã", "Banana", "Melão", "Melância", "Laranja"];
+
+for(let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
