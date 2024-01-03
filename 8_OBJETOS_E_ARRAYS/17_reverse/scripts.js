51cc7e5f9a107aad36e7d67beba88460cb6dd007
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-03 11:17:56 -0300

    funcionalidade executada, da aula 08

---
+++ 8_OBJETOS_E_ARRAYS/17_reverse/scripts.js
@@ -0,0 +1,14 @@
+// Criando um array de carros
+let carros = ["BMW", "Fiat", "VW", "Renault", "Audi"];
+
+// Revertendo a ordem dos elementos no array e imprimindo o array revertido
+console.log(carros.reverse());
+
+// Criando um array de números
+let nums = [1, 2, 3, 45, 6, 7, 7];
+
+// Revertendo a ordem dos elementos no array e imprimindo o array revertido
+console.log(nums.reverse());
+
+
+// apenas reverte os valores dentro da array
