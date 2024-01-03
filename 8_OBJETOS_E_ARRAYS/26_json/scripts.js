4303a78a881afefad9fe6be4aa66df8d8fc31847
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-03 12:11:49 -0300

    funcionalidade executada e atualizada

---
+++ 8_OBJETOS_E_ARRAYS/26_json/scripts.js
@@ -0,0 +1,11 @@
+// Criando um objeto chamado pessoa com propriedades nome e idade
+let pessoa = {
+  "nome": "Anderson",
+  "idade": 35
+}
+
+// Imprimindo o valor da propriedade "nome" do objeto pessoa
+console.log(pessoa.nome); // Saída: Anderson
+
+// Imprimindo o valor da propriedade "idade" do objeto pessoa
+console.log(pessoa.idade); // Saída: 35
