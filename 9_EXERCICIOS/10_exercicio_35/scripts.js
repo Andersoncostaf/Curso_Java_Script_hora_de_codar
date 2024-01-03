1167bff93cd96e28a07a475d91df89ec5827d566
Anderson Costa <andersoncosta1801@gmail.com>
2024-01-03 15:58:42 -0300

    funcionalidade executada e atualizada

---
+++ 9_EXERCICIOS/10_exercicio_35/scripts.js
@@ -0,0 +1,40 @@
+const calc = {
+  soma: function(a,b) {
+    return a + b;
+  },
+  subtrair: function(a,b) {
+    return a - b;
+  },
+  multiplicar: function(a,b) {
+    return a * b;
+  },
+  dividir: function(a,b) {
+    return a / b;
+  }
+}
+
+console.log(calc.soma(5,5));
+console.log(calc.subtrair(5,2));
+console.log(calc.multiplicar(10,2));
+console.log(calc.dividir(8,2));
+
+//outro
+const calculadora = {
+  soma: function(a, b) {
+    return a + b;
+  },
+  subtrair: function(a, b) {
+    return a - b;
+  },
+  multiplicar: function(a, b) {
+    return a * b;
+  },
+  dividir: function(a, b) {
+    return a / b;
+  }
+}
+
+console.log(calculadora.soma(3,4));
+console.log(calculadora.subtrair(5,5));
+console.log(calculadora.multiplicar(8,2));
+console.log(calculadora.dividir(5,2));
